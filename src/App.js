d70f4f9b94605d8cec58158249ed3a081d34fac6
Hermes Garcia <hermesgarcia@macbook-hermes.local>
2021-05-28 20:47:07 -0500

    First commmit of reactjs desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <>
+    <div className="main-banner img-container l-section" id="main-banner">
+        <div className="ed-grid lg-grid-6">
+            <div className="lg-cols-4 lg-x-2">
+                <img alt="Imagen" className="main-banner__img" src="https://i.pinimg.com/originals/ed/16/ea/ed16ea95b9471971cac69d9d4403d8ca.jpg" />
+                    <div className="main-banner__data s-center">
+                        <p className="t2 s-mb-0">Curso</p>
+                        <p>Tu futuro te está esperando</p>
+                        <a href="https://ed.team" className="button">Suscribirse</a>
+                    </div>
+            </div>
+        </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3">
+        <Curso />
+    </div>
+    </>
+)
 
 export default App;
