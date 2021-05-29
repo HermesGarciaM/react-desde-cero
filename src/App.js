ca5df873abb768709b6661688c86eec8c2ce8641
Hermes Garcia <hermesgarcia@macbook-hermes.local>
2021-05-28 21:50:14 -0500

    Showing arrays

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import './styles/styles.scss';
 import Curso from './Curso';
 
+const cursos = [
+    {
+        "title": "React desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/specialities/big/3e6a0de6-602b-439c-a90b-346f13c6760f.png",
+        "price": "10",
+        "profesor": "Hermes García"
+    },
+    {
+        "title": "Go desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/small/8b7ef439-b9bc-485f-9608-d82d0d1a7a2b.png",
+        "price": "20",
+        "profesor": "Hermes García"
+    },
+    {
+        "title": "Javascript desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/specialities/big/3e6a0de6-602b-439c-a90b-346f13c6760f.png",
+        "price": "30",
+        "profesor": "Hermes García"
+    },
+    {
+        "title": "Angular desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/small/8b7ef439-b9bc-485f-9608-d82d0d1a7a2b.png",
+        "price": "40",
+        "profesor": "Hermes García"
+    },
+    {
+        "title": "Magento desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/specialities/big/3e6a0de6-602b-439c-a90b-346f13c6760f.png",
+        "price": "50",
+        "profesor": "Hermes García"
+    },
+    {
+        "title": "HTML desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/small/8b7ef439-b9bc-485f-9608-d82d0d1a7a2b.png",
+        "price": "60",
+        "profesor": "Hermes García"
+    },
+]
 const App = () => (
     <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -17,7 +55,9 @@ const App = () => (
     </div>
 
     <div className="ed-grid m-grid-3">
-        <Curso />
+        {
+            cursos.map(curso => <Curso title={curso.title} image={curso.image} price={curso.price} profesor={curso.profesor} /> )
+        }
     </div>
     </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,36 @@
 import React from "react";
-
-const Curso = () => (
+import propTypes from 'prop-types'
+const Curso =  ({title, image, price, profesor}) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://ux.ed.team/img/curso-portada.png" alt="" />
+            <img src={image} alt={title} />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="t5 s-mb-2 s-center">
-                Programación orientada a objetos con Go
-            </h3>
-            <div className="s-mb-2 s-main-center">
-                <div className="card__teacher s-cross-center">
-                    <div className="card__avatar s-mr-1">
-                        <div className="circle img-container">
-                            <img src="https://ux.ed.team/img/curso-portada.png" alt="" />
-                        </div>
-                    </div>
-                    <span className="small">Alexys Lozada</span>
-                </div>
+            <div className="t5 s-mb-2 s-center">
+                <h3 className="center">{title}</h3>
             </div>
             <div className="s-main-center">
-                <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+                {`Prof.: ${profesor}`}
+            </div>
+            <div className="s-main-center">
+                <a className="button--ghost-alert button--tiny" href="#">{`$ ${price} USD`}</a>
             </div>
         </div>
     </article>
 )
+
+Curso.propTypes = {
+    title: propTypes.string,
+    image: propTypes.string,
+    price: propTypes.number,
+    profesor: propTypes.string
+}
+
+Curso.defaultProps = {
+    title: "No hay título",
+    image: "https://i.pinimg.com/originals/ed/16/ea/ed16ea95b9471971cac69d9d4403d8ca.jpg",
+    price: 0,
+    profesor: ""
+}
 
 export default Curso;
